bbbc2e833a13378b8cac8f06a4815480e24f12d9
Sharkbyteprojects <40953479+Sharkbyteprojects@users.noreply.github.com>
2019-03-20 18:57:40 +0100

    Update index.js

--- controls/index.js
+++ controls/index.js
@@ -1,6 +1,5 @@
 const Router = require('express').Router;
-const { listAction, onoff } = require('./controller')
+const { listAction } = require('./controller')
 router = Router();
 router.get('/', listAction)
-router.get('/onoff/:id', onoff)
 module.exports = router;
