9d1bd307049820e933ac4323d4f90db48970342c
- <->
2019-03-20 17:34:56 +0100

    Githublink

--- index.js
+++ index.js
@@ -7,6 +7,7 @@ const rawdate = require('./rawavail');
 app.use(express.static(__dirname + '/public'));
 app.use(morgan('common', { immediate: true }));
 app.get('/', (request, response) => response.redirect('/gpio'));
+app.get('/github', (request, response) => response.redirect('https://github.com/Sharkbyteprojects/RassI'));
 app.use('/gpio', routingcontrol);
 app.use('/gpio/raw', rawdate);
 app.listen(80, () => {
