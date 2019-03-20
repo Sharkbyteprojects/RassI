4d5b12d2f8772e48e86b610b9fc5f39b097bbf84
SHARKBYTE <-->
2019-03-20 15:11:24 +0100

    raw

--- index.js
+++ index.js
@@ -1,6 +1,11 @@
 const express = require('express');
+const routingcontrol = require('./controls')
 const app = express();
+const rawdate = require('./rawavail');
 
+app.get('/', (request, response) => response.redirect('/gpio'));
+app.use('/gpio', routingcontrol);
+app.use('/gpio/raw', rawdate);
 app.listen(80, () => {
     console.log("Server Listen on localhost Port 80");
 });
