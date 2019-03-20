76265797e9858b00ac162884648f9dbc668f84ff
SHARKBYTE <-->
2019-03-20 15:21:27 +0100

    add model

--- controls/controller.js
+++ controls/controller.js
@@ -1,18 +1,6 @@
-const gpios = [
-    {id: 2},
-    {id: 3},
-    {id: 4},
-    {id: 17},
-    {id: 27},
-    {id: 22},
-    {id: 5},
-    {id: 6},
-    {id: 13},
-    {id: 19},
-    {id: 26}
-];
+const model= require('./model');
 function listAction(request, response){
-    response.send(gpios);
+    response.send(model.getAll());
 }
 module.exports = {
     listAction,
---
+++ controls/model.js
@@ -0,0 +1,18 @@
+const gpios = [
+    {id: 2},
+    {id: 3},
+    {id: 4},
+    {id: 17},
+    {id: 27},
+    {id: 22},
+    {id: 5},
+    {id: 6},
+    {id: 13},
+    {id: 19},
+    {id: 26}
+];
+module.exports = {
+    getAll(){
+        return gpios;
+    },
+};
