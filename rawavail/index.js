3819a2fb9e6f059c64661a38ba7e1858cf73ba29
SHARKBYTE <-->
2019-03-20 15:35:03 +0100

    renew

--- rawavail/index.js
+++ rawavail/index.js
@@ -1,21 +1,9 @@
 const express = require('express');
 const routers = express.Router();
+const model = require('../controls/model');
 
-const gpios = [
-    {id: 2},
-    {id: 3},
-    {id: 4},
-    {id: 17},
-    {id: 27},
-    {id: 22},
-    {id: 5},
-    {id: 6},
-    {id: 13},
-    {id: 19},
-    {id: 26}
-];
 
 routers.get('/', (request, response) => {
-    response.send(gpios);
+    response.send(model.getAll());
 });
 module.exports = routers;
