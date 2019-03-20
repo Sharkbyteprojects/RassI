cfe9480cd36f5549aeb3f7c4a37f3956337ce774
SHARKBYTE <-->
2019-03-20 15:39:33 +0100

    add midleware

--- index.js
+++ index.js
@@ -1,8 +1,10 @@
 const express = require('express');
 const routingcontrol = require('./controls')
 const app = express();
+const morgan = require('morgan');
 const rawdate = require('./rawavail');
 
+app.use(morgan('common', { immediate: true }));
 app.get('/', (request, response) => response.redirect('/gpio'));
 app.use('/gpio', routingcontrol);
 app.use('/gpio/raw', rawdate);
